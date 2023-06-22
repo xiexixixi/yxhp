b0265e2d9de694d8b3439e433d9c75f6983136e6
Xie Yuning <xieyn12345@gmail.com>
2023-06-22 14:57:47 -0700

    finish skill component and experience

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -4,29 +4,31 @@ import Skill from './Skill'
 import {AiFillCheckCircle} from 'react-icons/ai'
 
 
-enum Proficiency {
-  learning = 'Learning',
-  basic = 'Basic',
-  intermediate = 'Intermediate',
-  experienced = 'Proficient',
-  expert = 'Expert',
+export enum Proficiency {
+  Learning = 'Learning',
+  Basic = 'Basic',
+  Intermediate = 'Intermediate',
+  Experienced = 'Proficient',
+  Expert = 'Expert',
+}
+enum Category {
+  Frontend = "Frontend",
+  Backend = "Backend",
+  Other = "Library&DevTools",
 }
 export interface SkillDetail{
   name: string;
   proficiency: Proficiency;
+  category: Category;
 }
 
 const skills:SkillDetail[] = [
-  {name: "HTML", proficiency:Proficiency.basic},
-  {name: "HTML", proficiency:Proficiency.basic},
-  {name: "HTML", proficiency:Proficiency.basic},
-  {name: "HTML", proficiency:Proficiency.basic},
-  {name: "HTML", proficiency:Proficiency.basic},
-  {name: "HTML", proficiency:Proficiency.basic},
-  {name: "HTML", proficiency:Proficiency.basic},
-  {name: "HTML", proficiency:Proficiency.basic},
-  {name: "HTML", proficiency:Proficiency.basic},
-  {name: "HTML", proficiency:Proficiency.basic},
+  {name: "HTML", proficiency:Proficiency.Learning, category:Category.Frontend},
+  {name: "HTML", proficiency:Proficiency.Basic, category:Category.Frontend},
+  {name: "HTML", proficiency:Proficiency.Basic, category:Category.Frontend},
+  {name: "HTML", proficiency:Proficiency.Basic, category:Category.Frontend},
+  {name: "HTML", proficiency:Proficiency.Basic, category:Category.Frontend},
+  {name: "HTML", proficiency:Proficiency.Basic, category:Category.Frontend},
 ]
 const Experience = () => {
   return (
@@ -34,12 +36,33 @@ const Experience = () => {
       <h5>What Skills I Have</h5>
       <h2>My Experience</h2>
       <div className="container experience__container">
-        <h3>Frontend Development</h3>
-        <div className="experience__content">
-          <div>{skills.map((sk, index) => (
-        <Skill key={index} name={sk.name} proficiency={sk.proficiency} />
-      ))}</div>
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            {skills.map((sk, index) => (
+              <Skill key={index} name={sk.name} proficiency={sk.proficiency} category={Category.Frontend}/>
+            ))}
+          </div>
         </div>
+
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            {skills.map((sk, index) => (
+              <Skill key={index} name={sk.name} proficiency={sk.proficiency} category={Category.Frontend}/>
+            ))}
+          </div>
+        </div>
+
+        <div className="experience__other">
+          <h3>Library & Dev Tools</h3>
+          <div className="experience__content">
+            {skills.map((sk, index) => (
+              <Skill key={index} name={sk.name} proficiency={sk.proficiency} category={Category.Frontend}/>
+            ))}
+          </div>
+        </div>
+
       </div>
     </section>
   )
--- src/components/experience/Skill.tsx
+++ src/components/experience/Skill.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import './skill.css'
 import {AiFillCheckCircle} from 'react-icons/ai'
-import { SkillDetail } from './Experience'
+import { SkillDetail, Proficiency } from './Experience'
+
+import {FaBookReader} from 'react-icons/fa'
 
 const Skill:React.FC<SkillDetail> = (props) => {
   return (
-    <div>
-        <AiFillCheckCircle/>
-        <h3>{props.name}</h3>
-        <h5>{props.proficiency}</h5>
+    <div className='skilldetail'>
+        {props.proficiency===Proficiency.Learning?
+        <FaBookReader className='skilldetail__icon'/>:
+        <AiFillCheckCircle className='skilldetail__icon'/>}
+
+        <div className='skilldetail__text'>
+          <h3>{props.name}</h3>
+          <h5>{props.proficiency}</h5>
+        </div>
     </div>
   )
 }
