2c75c26c3b77beaf30b82ef086bea9f633e21c5d
Xie Yuning <xieyn12345@gmail.com>
2023-06-21 23:13:35 -0700

    nav

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './nav.css'
+import {AiOutlineHome, AiOutlineUser, AiOutlineBook} from 'react-icons/ai'
+import {BiMessageSquareDetail} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+
+
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#' className={activeNav==='#'?'active':''} onClick={()=>setActiveNav('#')}><AiOutlineHome size='2rem'/></a>
+      <a href='#about' className={activeNav==='#about'?'active':''} onClick={()=>setActiveNav('#about')}><AiOutlineUser size='2rem'/></a>
+      <a href='#experience' className={activeNav==='#experience'?'active':''} onClick={()=>setActiveNav('#experience')}><AiOutlineBook size='2rem'/></a>
+      <a href='#services' className={activeNav==='#services'?'active':''} onClick={()=>setActiveNav('#services')}><RiServiceLine size='2rem'/></a>
+      <a href='#contact' className={activeNav==='#contact'?'active':''} onClick={()=>setActiveNav('#contact')}><BiMessageSquareDetail size='2rem'/></a>
+    </nav>
   )
 }
 
