9bd5df1d195b2e514ccc7228bbc9dd3e64c3e569
Xie Yuning <xieyn12345@gmail.com>
2023-06-27 00:07:57 -0700

    portfolio update

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,22 +1,47 @@
 import React from 'react'
-import './portfolio.css'
 import { styled } from 'styled-components'
+import pok from '../../assets/pok.png'
+
 import PortfolioProject from './PortfolioProject'
 
-const PortfoiloContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 2.5rem;
-`
 
-const Portfolio = () => {
+export interface ProjectDetail {
+  picture: string,
+  title: string,
+  description: string,
+  skills: string[],
+  demoLink: string,
+  sourceLink: string
+}
+const projects:ProjectDetail[] = [
+  {
+    picture: pok,
+    title: 'BillShare', 
+    description: 'A mobile app to manage shared bills with friends', 
+    skills:['React Typescript','Spring Boot','Spring Data JPA', 'MySQL', 'WebSocket', 'Redis', 'AWS'],
+    demoLink: 'https://www.youtube.com/watch?v=D1gZhuITnMw&t=5s',
+    sourceLink: 'https://github.com/darlingof02/billshare_frontend',
+  },
+  
+  {
+    picture: pok,
+    title: 'TapNews', 
+    description: 'A web app that displays news collected from various news sources', 
+    skills:['React','Flask','MongoDB', 'Redis', 'RabbitMQ '],
+    demoLink: 'https://www.youtube.com/watch?v=9Jdp9YM9IIk',
+    sourceLink: 'https://github.com/darlingof02/295p-team1-tapnews-main',
+  },
+
+]
+
+const Portfolio:React.FC = () => {
   return (
-    <section id='portfolio'>Portfolio
+    <section id='portfolio'>
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
       <PortfoiloContainer className='container'>
-        <PortfolioProject/>
+        {projects.map((pd) => <PortfolioProject {...pd}/>)}
       </PortfoiloContainer>
     
     </section>
@@ -24,3 +49,20 @@ const Portfolio = () => {
 }
 
 export default Portfolio
+
+const PortfoiloContainer = styled.div`
+  /* width: var(--container-width-lg); */
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2.5rem;
+
+  @media screen and (max-width: 1024px){
+    width: 60%;
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media screen and (max-width: 600px){
+    width: 50%;
+    grid-template-columns: repeat(1, 1fr);
+  }
+`
--- src/components/portfolio/PortfolioProject.tsx
+++ src/components/portfolio/PortfolioProject.tsx
@@ -1,42 +1,48 @@
 import React from 'react'
 import pok from '../../assets/pok.png'
 import { styled } from 'styled-components'
+import {ProjectDetail} from './Portfolio'
 
 const Project = styled.div`
+    /* position: relative; */
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
     background: var(--color-bg-variant);
     /* padding: 1.2rem; */
-    border-radius: 2rem;
-    border: 1px solid transparent;
+    border-radius: 1rem;
+    border: 3px solid transparent;
     transition: var(--transition);
+    box-shadow: outset 0 1rem 3rem rgba(0, 0, 0, 0.3), 0 1rem rgba(255, 255, 255, 0.1);
     &:hover {
-        border-color: var(--color-primary-variant);
-        background: transparent;
-    }
-    & h3 {
-        margin: 1.2rem 0 2rem;
+        scale: 1.1;
     }
 `
 const ProjectImage = styled.img`
-    border-radius: 2rem 2rem 1rem 1rem;
+    border-radius: 1rem 1rem 0rem 0rem;
     overflow: hidden;
 `
 const ButtonContainer = styled.div`
-    display: flex;
+    /* position: absolute; */
+    margin-top: 1rem;
     gap: 1rem;
     margin: 1rem 1rem;
+    display: flex;
     justify-content: center;
+    bottom: 0rem;
 
 `
 
 
-function PortfolioProject() {
+const PortfolioProject:React.FC<ProjectDetail> = (props) =>{
   return (
     <Project>
-        <ProjectImage src={pok}/>
-        <h3 style={{margin: '1rem'}}>Title</h3>
+        <ProjectImage src={props.picture}/>
+        <h3 style={{marginTop: '1rem', textAlign: 'center'}}>{props.title}</h3>
+        <h5 style={{margin: '0 2rem 1rem 2rem', color: 'var(--color-light)'}}>{"  "+props.description}</h5>
         <ButtonContainer>
-            <a href='#' className='btn' target='_blank'>Demo</a>
-            <a href='https://github.com' className='btn' target='_blank'>Github</a>
+            <a href={props.demoLink} className='btn' target='_blank'>Demo</a>
+            <a href={props.sourceLink} className='btn' target='_blank'>Github</a>
         </ButtonContainer>
     </Project>
   )
