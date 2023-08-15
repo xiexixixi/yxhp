a9eeee8263f0c1c1e37eb483f1b274f8b951d630
Xie Yuning <xieyn12345@gmail.com>
2023-08-14 23:14:33 -0700

    add portfolio

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,65 +1,72 @@
-import React from 'react'
-import { styled } from 'styled-components'
-import billshare from '../../assets/billsharedemo.png'
-import tapnews from '../../assets/tapnews.png'
+import React from "react";
+import { styled } from "styled-components";
+import billshare from "../../assets/billsharedemo.png";
+import tapnews from "../../assets/tapnews.png";
 
-import view from '../../assets/morraine-lake.jpg'
+import view from "../../assets/morraine-lake.jpg";
+import GAILL from "../../assets/GAILL.png";
 
-import PortfolioProject from './PortfolioProject'
-
+import PortfolioProject from "./PortfolioProject";
 
 export interface ProjectDetail {
-  picture?: string,
-  title: string,
-  description: string,
-  skills: string[],
-  demoLink?: string,
-  sourceLink?: string
+  picture?: string;
+  title: string;
+  description: string;
+  skills: string[];
+  demoLink?: string;
+  sourceLink?: string;
 }
-const projects:ProjectDetail[] = [
+const projects: ProjectDetail[] = [
+  {
+    picture: GAILL,
+    title: "EFAS-GAILL",
+    description: "Generate-Based AI Leak Locator",
+    skills: ["TypeScript", "React", "OpenLayers"],
+    
+  },
   {
     picture: billshare,
-    title: 'BillShare', 
-    description: 'A mobile app to manage shared bills with friends', 
-    skills:['React Typescript','Spring Boot','Spring Data JPA', 'MySQL', 'WebSocket', 'Redis', 'AWS'],
-    demoLink: 'https://www.youtube.com/watch?v=D1gZhuITnMw&t=5s',
-    sourceLink: 'https://github.com/darlingof02/billshare_frontend',
+    title: "BillShare",
+    description: "A mobile app to manage shared bills with friends",
+    skills: ["React Typescript", "Spring Boot", "Spring Data JPA", "MySQL", "WebSocket", "Redis", "AWS"],
+    demoLink: "https://www.youtube.com/watch?v=D1gZhuITnMw&t=5s",
+    sourceLink: "https://github.com/darlingof02/billshare_frontend",
   },
-  
+
   {
     picture: tapnews,
-    title: 'TapNews', 
-    description: 'A web app that displays news collected from various news sources', 
-    skills:['React','Flask','MongoDB', 'Redis', 'RabbitMQ'],
-    demoLink: 'https://www.youtube.com/watch?v=9Jdp9YM9IIk',
-    sourceLink: 'https://github.com/darlingof02/295p-team1-tapnews-main',
+    title: "TapNews",
+    description: "A web app that displays news collected from various news sources",
+    skills: ["React", "Flask", "MongoDB", "Redis", "RabbitMQ"],
+    demoLink: "https://www.youtube.com/watch?v=9Jdp9YM9IIk",
+    sourceLink: "https://github.com/darlingof02/295p-team1-tapnews-main",
   },
 
   {
     picture: view,
-    title: 'Sports Process Analysis ', 
-    description: 'An automatic highlight editor for tennis matches', 
-    skills:['PyTorch','OpenSMILE ','OpenCV'],
-    sourceLink: 'https://github.com/xiexixixi/Sports_process_analysis',
+    title: "Sports Process Analysis ",
+    description: "An automatic highlight editor for tennis matches",
+    skills: ["PyTorch", "OpenSMILE ", "OpenCV"],
+    sourceLink: "https://github.com/xiexixixi/Sports_process_analysis",
   },
+];
 
-]
-
-const Portfolio:React.FC = () => {
+const Portfolio: React.FC = () => {
   return (
-    <section id='portfolio'>
+    <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
-      <PortfoiloContainer className='container'>
-        {projects.map((pd,idx) => <PortfolioProject key={idx} {...pd}/>)}
+      <PortfoiloContainer className="container">
+        {projects.map((pd, idx) => (
+          <PortfolioProject key={idx} {...pd} />
+        ))}
       </PortfoiloContainer>
-    
     </section>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
 
 const PortfoiloContainer = styled.div`
   /* width: var(--container-width-lg); */
@@ -68,12 +75,12 @@ const PortfoiloContainer = styled.div`
   grid-template-columns: repeat(3, 1fr);
   gap: 2.5rem;
 
-  @media screen and (max-width: 1024px){
+  @media screen and (max-width: 1024px) {
     width: 60%;
     grid-template-columns: repeat(2, 1fr);
   }
-  @media screen and (max-width: 600px){
+  @media screen and (max-width: 600px) {
     width: 50%;
     grid-template-columns: repeat(1, 1fr);
   }
-`
+`;
--- src/components/portfolio/PortfolioProject.tsx
+++ src/components/portfolio/PortfolioProject.tsx
@@ -1,58 +1,62 @@
-import React from 'react'
-import { styled } from 'styled-components'
-import {ProjectDetail} from './Portfolio'
+import React from "react";
+import { styled } from "styled-components";
+import { ProjectDetail } from "./Portfolio";
 
 const Project = styled.div`
-    /* position: relative; */
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    background: var(--color-bg-variant);
-    /* padding: 1.2rem; */
-    border-radius: 1rem;
-    border: 2px solid transparent;
-    transition: var(--transition);
-    box-shadow: outset 0 1rem 3rem rgba(0, 0, 0, 0.3), 0 1rem rgba(255, 255, 255, 0.1);
-    &:hover {
-        scale: 1.1;
-    }
-`
+  /* position: relative; */
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background: var(--color-bg-variant);
+  /* padding: 1.2rem; */
+  border-radius: 1rem;
+  border: 2px solid transparent;
+  transition: var(--transition);
+  box-shadow: outset 0 1rem 3rem rgba(0, 0, 0, 0.3), 0 1rem rgba(255, 255, 255, 0.1);
+  &:hover {
+    scale: 1.1;
+  }
+`;
 const ProjectImage = styled.img`
-
-    height: 25vh;
-    border-radius: 1rem 1rem 0rem 0rem;
-    overflow: hidden;
-`
+  height: 25vh;
+  border-radius: 1rem 1rem 0rem 0rem;
+  overflow: hidden;
+`;
 
 interface buttondisplay {
-    display: string,
+  display: string;
 }
 const ButtonContainer = styled.div<buttondisplay>`
-    /* position: absolute; */
-    margin-top: 1rem;
-    gap: 1rem;
-    margin: 1rem 1rem;
-    display: flex;
-    justify-content: center;
-    bottom: 0rem;
-    & .demo__link {
-        display: ${props=>props.display};
-    }
-`
+  /* position: absolute; */
+  margin-top: 1rem;
+  gap: 1rem;
+  margin: 1rem 1rem;
+  display: flex;
+  justify-content: center;
+  bottom: 0rem;
+  & .demo__link {
+    display: ${(props) => props.display};
+  }
+`;
 
-
-const PortfolioProject:React.FC<ProjectDetail> = (props) =>{
+const PortfolioProject: React.FC<ProjectDetail> = (props) => {
   return (
     <Project>
-        <ProjectImage src={props.picture}/>
-        <h3 style={{marginTop: '1rem', textAlign: 'center'}}>{props.title}</h3>
-        <h5 style={{margin: '0 2rem 1rem 2rem', color: 'var(--color-light)'}}>{"  "+props.description}</h5>
-        <ButtonContainer display={props.demoLink==null || ''?String('none'):String('')}>
-            <a href={props.demoLink} className='btn demo__link' target='_blank' rel="noreferrer">Demo</a>
-            <a href={props.sourceLink} className='btn source__link' target='_blank' rel="noreferrer">Github</a>
-        </ButtonContainer>
+      <ProjectImage src={props.picture} />
+      <h3 style={{ marginTop: "1rem", textAlign: "center" }}>{props.title}</h3>
+      <h5 style={{ margin: "0 2rem 1rem 2rem", color: "var(--color-light)" }}>{"  " + props.description}</h5>
+      <ButtonContainer display={props.demoLink == null || "" ? String("none") : String("")}>
+        <a href={props.demoLink} className="btn demo__link" target="_blank" rel="noreferrer">
+          Demo
+        </a>
+        {!!props.sourceLink && (
+          <a href={props.sourceLink} className="btn source__link" target="_blank" rel="noreferrer">
+            Github
+          </a>
+        )}
+      </ButtonContainer>
     </Project>
-  )
-}
+  );
+};
 
-export default PortfolioProject
+export default PortfolioProject;
